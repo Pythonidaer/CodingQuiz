3ed7c70fc63cb6ee8b2ff12847bc1ad04bfacb31
Pythonidaer <pythonidaer@gmail.com>
2021-01-03 22:35:08 -0500

    Added Highscores and Time: 0 to index.html and also researched what needs to happen next.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CodingQuiz
 *Coding Quiz utilizing JavaScript and localStorage*
 
-[View Deployed Application Here.]()
+[View Deployed Application Here.](https://pythonidaer.github.io/CodingQuiz/)
 
 ## Installation
 1. Clone repository to your local device
@@ -14,6 +14,10 @@ This repo is simply a homework assignment. Feel free to inspect the code and not
   
 ## Credits
 [Stack Overflow](https://stackoverflow.com/questions/16562577/how-can-i-make-a-button-redirect-my-page-to-another-page) How to make my button re-direct to a new page.
+
+[Brian Design](https://www.youtube.com/watch?v=f4fB9Xg2JEY) This tutorial helped me understand how to make the quiz start on a second page.
+
+[GitHub: Brian Codex](https://github.com/briancodex/quiz-app-js) Accompanying code to the Brian Design video.
   
 ## License
 I've chosen an MIT License. Do what you'd like with this material.
--- assets/scripts/behavior.js
+++ assets/scripts/behavior.js
@@ -3,9 +3,16 @@
 var startBtn = document.getElementById('start');
 
 startBtn.addEventListener('click', function() {
-    // This needs to be changed for when the website is live.
-    let homePage = 'file:///C:/Users/jonam/CodingQuiz/index.html';
-    let quizPage = 'file:///C:/Users/jonam/CodingQuiz/quiz.html';
+    // For testing on local machine.
+    // let homePage = 'file:///C:/Users/jonam/CodingQuiz/index.html';
+    // let quizPage = 'file:///C:/Users/jonam/CodingQuiz/quiz.html';
+
+    // For production.
+    let homePage = 'https://pythonidaer.github.io/CodingQuiz/index.html';
+    let quizPage = 'https://pythonidaer.github.io/CodingQuiz/quiz.html';
+
+    // https://pythonidaer.github.io/CodingQuiz/index.html
+    // https://pythonidaer.github.io/CodingQuiz/quiz.html
 
     // (if location is index.html) then (location is quiz.html) otherwise (location is index.html)
     (location.href === homePage) && (location.href = quizPage) || (location.href = homePage);
